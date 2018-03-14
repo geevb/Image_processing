078f16772ca472636da63d18a8789d17667c248b
Getulio Beninca <getuliovb@gmail.com>
2018-03-14 17:04:24 -0300

    Conversao de cores

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,6 +1,52 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 using namespace cv;
+
+void converterParaCinza(Mat& image) {
+    for(int x = 0; x < image.size().width; x++) {
+        for(int y = 0; y < image.size().height; y++) {
+            image.at<Vec3b>(Point(x, y))[0] = image.at<Vec3b>(y, x)[0] * 0.114; // blue
+            image.at<Vec3b>(Point(x, y))[1] = image.at<Vec3b>(y, x)[1] * 0.587; // green
+            image.at<Vec3b>(Point(x, y))[2] = image.at<Vec3b>(y, x)[2] * 0.299; // red
+        } 
+    }
+}
+
+void converterParaCorInvertida(Mat& image) {
+    for(int x = 0; x < image.size().width; x++) {
+        for(int y = 0; y < image.size().height; y++) {
+            image.at<Vec3b>(Point(x, y))[0] = 255 - image.at<Vec3b>(y, x)[0]; // blue
+            image.at<Vec3b>(Point(x, y))[1] = 255 - image.at<Vec3b>(y, x)[1]; // green
+            image.at<Vec3b>(Point(x, y))[2] = 255 - image.at<Vec3b>(y, x)[2]; // red
+        } 
+    }
+}
+
+void converterParaLimiar(Mat& image, int limiar){
+    for(int x = 0; x < image.size().width; x++) {
+        for(int y = 0; y < image.size().height; y++) {
+            if(image.at<Vec3b>(Point(x, y))[0] > limiar) {
+                image.at<Vec3b>(Point(x, y))[0] = 1;
+            } else {
+                image.at<Vec3b>(Point(x, y))[0] = 0;
+            }
+
+            if(image.at<Vec3b>(Point(x, y))[1] > limiar) {
+                image.at<Vec3b>(Point(x, y))[1] = 1;
+            } else {
+                image.at<Vec3b>(Point(x, y))[1] = 0;
+            }
+
+            if(image.at<Vec3b>(Point(x, y))[2] > limiar) {
+                image.at<Vec3b>(Point(x, y))[2] = 1;
+            } else {
+                image.at<Vec3b>(Point(x, y))[2] = 0;
+            }
+        } 
+    }
+}
+
+
 int main(int argc, char** argv )
 {
     if ( argc != 2 )
@@ -15,22 +61,12 @@ int main(int argc, char** argv )
         printf("No image data \n");
         return -1;
     }
-    // namedWindow("Display Image", WINDOW_AUTOSIZE );
-    // imshow("Display Image", image);
-    // waitKey(0);
-    
-    for(int x = 0; x < image.size().width; x++) {
-        for(int y = 0; y < image.size().height; y++) {
-            Vec3b intensity = image.at<Vec3b>(y, x);
-            uchar blue = intensity.val[0];
-            uchar green = intensity.val[1];
-            uchar red = intensity.val[2];
-            std::cout << +blue << std::endl;
-            std::cout << +green << std::endl;
-            std::cout << +red << std::endl;
-        } 
-    }
 
+    converterParaCinza(image);
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+    waitKey(0);
 
     return 0;
 }
+
