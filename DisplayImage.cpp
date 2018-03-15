fec056344694c01d5e0e84e1bbc9e584bacb88d0
Getúlio Benincá <getuliovb@gmail.com>
2018-03-14 22:09:10 -0300

    Incrementar canais

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -94,6 +94,52 @@ Mat isolarCanalDeCor(std::string cor, Mat image) {
     return newImage;
 }
 
+Mat incrementarCanaisDeDor(std::string cor, int tipo, int valor, Mat image) {
+    // Tipo 0: Inteiro, 1: Percentual
+    Mat newImage(image.size().height, image.size().width, CV_8UC3, Scalar(0,0,0));
+    for(int y = 0; y < image.size().height; y++) {
+        for(int x = 0; x < image.size().width; x++) {
+            Vec<unsigned char, 3>& oldImagePixel = image.at<Vec3b>(Point(x, y));       
+            Vec<unsigned char, 3>& newImagePixel = newImage.at<Vec3b>(Point(x, y));
+
+            if(cor == "B") {
+                double novoValor = (tipo == 0) ? oldImagePixel[0] + valor : ( oldImagePixel[0] + (oldImagePixel[0] * (valor / 100)));
+                newImagePixel[0] = (novoValor <= 255) ? novoValor : 255;
+                newImagePixel[1] = oldImagePixel[1];
+                newImagePixel[2] = oldImagePixel[2];
+            }
+            
+            if(cor == "G") {
+                double novoValor = (tipo == 0) ? oldImagePixel[1] + valor : ( oldImagePixel[1] + (oldImagePixel[1] * (valor / 100)));
+                newImagePixel[0] = oldImagePixel[0]; 
+                newImagePixel[1] = (novoValor <= 255) ? novoValor : 255;
+                newImagePixel[2] = oldImagePixel[2];
+            }
+
+            if(cor == "R") {
+                double novoValor = (tipo == 0) ? oldImagePixel[2] + valor : ( oldImagePixel[2] + (oldImagePixel[2] * (valor / 100)));
+                newImagePixel[0] = oldImagePixel[0];
+                newImagePixel[1] = oldImagePixel[1];
+                newImagePixel[2] = (novoValor > 255) ? 255 : novoValor;
+            }
+        }      
+    }
+
+    return newImage;
+}
+
+Mat zoomIn(int zoomValue, Mat image) {
+    Mat newImage(image.size().height * zoomValue, image.size().width * zoomValue, CV_8UC3, Scalar(0,0,0));
+    for(int y = 0; y < image.size().height; y++) {
+        for(int x = 0; x < image.size().width; x++) {
+            // Vec<unsigned char, 3>& oldImagePixel = image.at<Vec3b>(Point(x, y));       
+            // Vec<unsigned char, 3>& newImagePixel = newImage.at<Vec3b>(Point(x, y));
+            std::cout << "KEK";
+        }
+    }
+
+    return newImage;
+}
 
 int main(int argc, char** argv )
 {
@@ -114,7 +160,9 @@ int main(int argc, char** argv )
     // Mat transformedImage = converterParaCinza(image);
     // Mat transformedImage = converterParaCorInvertida(image);
     // Mat transformedImage = converterParaLimiar(image, 50);
-    Mat transformedImage = isolarCanalDeCor("R", image);
+    // Mat transformedImage = isolarCanalDeCor("R", image);
+    // Mat transformedImage = incrementarCanaisDeDor("R", 0, 255, image);
+    Mat transformedImage = zoomIn(2, image);
 
 
 
