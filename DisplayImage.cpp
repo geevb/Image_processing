3e1e3af8965c34357a7fb3f6f24637333ab4d201
Getúlio Benincá <getuliovb@gmail.com>
2018-03-15 21:18:43 -0300

    Subtracao

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -33,6 +33,7 @@ Mat converterParaCorInvertida(Mat image) {
     return newImage;
 }
 
+// TODO: REVISAR
 Mat converterParaLimiar(Mat image, int limiar) {
     Mat newImage;
     image.copyTo(newImage);
@@ -129,16 +130,52 @@ Mat incrementarCanaisDeDor(std::string cor, int tipo, int valor, Mat image) {
 }
 
 Mat zoomIn(int zoomValue, Mat image) {
+    if (zoomValue == 1) return image;
+
     Mat newImage(image.size().height * zoomValue, image.size().width * zoomValue, CV_8UC3, Scalar(0,0,0));
-    for(int y = 0; y < image.size().height; y++) {
-        for(int x = 0; x < image.size().width; x++) {
-            // Vec<unsigned char, 3>& oldImagePixel = image.at<Vec3b>(Point(x, y));       
-            // Vec<unsigned char, 3>& newImagePixel = newImage.at<Vec3b>(Point(x, y));
-            std::cout << "KEK";
+    for(int y = 0; y < newImage.size().height; y += zoomValue) {
+        for(int x = 0; x < newImage.size().width; x += zoomValue ) {
+            Vec<unsigned char, 3>& oldImagePixel = image.at<Vec3b>(Point(x, y));       
+            Vec<unsigned char, 3>& newImagePixel = newImage.at<Vec3b>(Point(x, y));
+
+            for(int z = 0; z < zoomValue; z++) {
+                for(int w = 0; w < zoomValue; w ++) {
+                    newImage.at<Vec3b>(Point(z, w)) = image.at<Vec3b>(Point(x, y));
+                }
+                // newImage.at<Vec3b>(Point(x, y));
+                // y0x0m, y0x1, y1x0, y1x1
+            }
+
         }
     }
 
     return newImage;
+}
+
+Mat subtrairImagem(Mat imgMinuendo, Mat imgSubtraendo) {
+    Mat newImg(imgMinuendo.size().height, imgMinuendo.size().width, CV_8UC3, Scalar(0,0,0));
+    for(int y = 0; y < newImg.size().height; y++) {
+        for(int x = 0; x < newImg.size().width; x++) {
+            Vec<unsigned char, 3>& newImgPixel = newImg.at<Vec3b>(Point(x, y));
+            Vec<unsigned char, 3>& pxlMinuendo = imgMinuendo.at<Vec3b>(Point(x, y));
+            Vec<unsigned char, 3>& pxlSubtraendo = imgSubtraendo.at<Vec3b>(Point(x, y));
+            if(x > imgSubtraendo.size().width || y > imgSubtraendo.size().height) {
+                newImgPixel[0] = pxlMinuendo[0];
+                newImgPixel[1] = pxlMinuendo[1];
+                newImgPixel[2] = pxlMinuendo[2];
+                continue;
+            }
+
+            int sub0 = pxlMinuendo[0] - pxlSubtraendo[0];
+            int sub1 = pxlMinuendo[1] - pxlSubtraendo[1];
+            int sub2 = pxlMinuendo[2] - pxlSubtraendo[2];
+            newImgPixel[0] = (sub0 < 0) ? 0 : sub0;
+            newImgPixel[1] = (sub1 < 0) ? 0 : sub1;
+            newImgPixel[2] = (sub2 < 0) ? 0 : sub2;
+        }
+    }
+    
+    return newImg;
 }
 
 int main(int argc, char** argv )
@@ -151,7 +188,10 @@ int main(int argc, char** argv )
 
     Mat image;
     image = imread( argv[1], 1 );
-    if ( !image.data )
+
+    Mat image2;
+    image2 = imread("black.png");
+    if ( !image.data || !image2.data )
     {
         printf("No image data \n");
         return -1;
@@ -162,7 +202,8 @@ int main(int argc, char** argv )
     // Mat transformedImage = converterParaLimiar(image, 50);
     // Mat transformedImage = isolarCanalDeCor("R", image);
     // Mat transformedImage = incrementarCanaisDeDor("R", 0, 255, image);
-    Mat transformedImage = zoomIn(2, image);
+    // Mat transformedImage = zoomIn(2, image);
+    Mat transformedImage = subtrairImagem(image, image2);
 
 
 
